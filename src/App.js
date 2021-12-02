d31bcb42943e3ee3d4acf37012ef31f884036108
BPDanek <danekbenjamin@gmail.com>
2021-12-01 22:27:27 -0800

    checking & launch on Vercel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function Form() {
+    const { register, handleSubmit } = useForm();
+
+    return <form onSubmit={
+        handleSubmit(data => {
+            const url = `http://localhost:3000/query-pur-db?counties=${data.counties}`
+            fetch(url, {
+                method: "GET",
+                mode: "cors"
+            })
+                .then(response => response.json())
+                .then(response => {console.log("response", response)})
+                .catch(error => {console.log("error", error)})
+        })}>
+        <label>Field</label>
+        <input name="Counties: " {...register('counties')} />
+        <input type="submit" />
+    </form>;
 }
-
-export default App;
