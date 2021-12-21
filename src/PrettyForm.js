9fb9b1033439c49f9b39178c48002acef46decf8
BPDanek <danekbenjamin@gmail.com>
2021-12-20 19:31:53 -0800

    bump vercel

--- src/PrettyForm.js
+++ src/PrettyForm.js
@@ -8,7 +8,6 @@ import CardContent from "@material-ui/core/CardContent"
 import CardActions from "@material-ui/core/CardActions"
 import Button from "@material-ui/core/Button"
 
-
 export default class PrettyForm extends React.Component {
     constructor(props) {
         super(props)
